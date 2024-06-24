305e61cc90b0af7f8c162e6958bbff4d2b6e5eef
Ajay Sharma <ajaysharma502074@gmail.com>
2024-06-24 13:49:56 +0530

    without fixtures

--- source/shapes.py
+++ source/shapes.py
@@ -18,6 +18,18 @@ class circle(shape):
         return math.pi * self.radius ** 2
 
     def perimeter(self):
-        return 2*math.pi*self.radius
+        return 2 * math.pi * self.radius
 
-    
+
+class Rectangle(shape):
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2*(self.width+self.length)
+
