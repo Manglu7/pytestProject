465322e1219ccd18e2fb0b0491c69f310ff5de8a
Ajay Sharma <ajaysharma502074@gmail.com>
2024-06-25 18:41:46 +0530

    mocking

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 import source.my_functions as my_functions
 
@@ -20,3 +21,20 @@ def test_add_strings():
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         my_functions.divide(10, 0)
+
+
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_functions.divide(2, 2)
+    assert (result == 1)
+
+
+@pytest.mark.skip(reason = 'this is a broken feature currently')
+def test_add():
+    assert my_functions.add(1, 2) == 3
+
+
+@pytest.mark.xfail(reason = 'we know we cannot divide by zero')
+def test_divide_by_zer_broken():
+    my_functions.divide(2, 0)
